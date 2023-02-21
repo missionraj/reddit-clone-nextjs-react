edf58ce350e97248bbab5bf78b3f023ae75ae739
raj rana <raj.rana507@gmail.com>
2023-02-21 10:15:42 +0530

    feat : style changes for the search input

--- src/components/Navbar/SearchInput.tsx
+++ src/components/Navbar/SearchInput.tsx
@@ -8,12 +8,29 @@ type SearchInputProps = {
 
 const SearchInput: React.FC<SearchInputProps> = () => {
   return (
-    <Flex>
+    <Flex flexGrow={1} mr={2} align="center">
       <InputGroup>
         <InputLeftElement pointerEvents="none">
-          <SearchIcon color="gray.300" />
+          <SearchIcon color="gray.300" mb={1} />
         </InputLeftElement>
-        <Input type="tel" placeholder="Phone number" />
+        <Input
+          placeholder="Search Reddit"
+          _placeholder={{
+            color: "gray.500",
+          }}
+          _hover={{
+            bg: "white",
+            border: "1px solid",
+            borderColor: "blue.500",
+          }}
+          _focus={{
+            outline: "none",
+            border: "1px solid",
+            borderColor: "blue.500",
+          }}
+          height="34px"
+          bg="gray.50"
+        />
       </InputGroup>
     </Flex>
   );
